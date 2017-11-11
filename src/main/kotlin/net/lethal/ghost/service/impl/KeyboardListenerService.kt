3bed0fe1d8785d0796bfd2d849eef7b49e1184f7
lujo5 <lukamatosevic5@gmail.com>
2017-11-11 17:46:24 +0100

    Added jar building with dependencies. Additional key mappings added.

--- src/main/kotlin/net/lethal/ghost/service/impl/KeyboardListenerService.kt
+++ src/main/kotlin/net/lethal/ghost/service/impl/KeyboardListenerService.kt
@@ -35,7 +35,6 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
         val finalCode = toFinalKeyCode(event!!)
         if (paused || keyMapPressedTimestamp.containsKey(finalCode)) return
         keyMapPressedTimestamp.put(finalCode, System.currentTimeMillis())
-        logger.info("press keyCode=${event.keyCode} rawCode=${event.rawCode} finalCode=$finalCode")
         notifySubscribers(KeyboardEvent(order.getAndIncrement(), 50, KeyPressedAction(finalCode)))
     }
 
@@ -43,7 +42,6 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
         val finalCode = toFinalKeyCode(event!!)
         if (paused || !keyMapPressedTimestamp.containsKey(finalCode)) return
         keyMapPressedTimestamp.remove(finalCode)
-        logger.info("released keyCode=${event.keyCode} rawCode=${event.rawCode} finalCode=$finalCode")
         notifySubscribers(KeyboardEvent(order.getAndIncrement(), 50, KeyReleasedAction(finalCode)))
     }
 
@@ -53,6 +51,7 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
 
     private fun toFinalKeyCode(event: NativeKeyEvent): Int {
         return when (event.keyCode) {
+        // Action keys
             NativeKeyEvent.VC_ENTER -> KeyEvent.VK_ENTER
             NativeKeyEvent.VC_SHIFT -> KeyEvent.VK_SHIFT
             NativeKeyEvent.VC_BACKSPACE -> KeyEvent.VK_BACK_SPACE
@@ -61,7 +60,7 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
             NativeKeyEvent.VC_TAB -> KeyEvent.VK_TAB
             NativeKeyEvent.VC_CAPS_LOCK -> KeyEvent.VK_CAPS_LOCK
             NativeKeyEvent.VC_ESCAPE -> KeyEvent.VK_ESCAPE
-            NativeKeyEvent.VC_PRINTSCREEN -> if (event.rawCode == 44) KeyEvent.VK_PRINTSCREEN else event.rawCode
+            NativeKeyEvent.VC_PRINTSCREEN -> if (event.rawCode == 44) KeyEvent.VK_PRINTSCREEN else KeyEvent.VK_MULTIPLY
             NativeKeyEvent.VC_SCROLL_LOCK -> KeyEvent.VK_SCROLL_LOCK
             NativeKeyEvent.VC_PAUSE -> KeyEvent.VK_PAUSE
             NativeKeyEvent.VC_INSERT -> KeyEvent.VK_INSERT
@@ -71,6 +70,8 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
             NativeKeyEvent.VC_END -> KeyEvent.VK_END
             NativeKeyEvent.VC_PAGE_DOWN -> KeyEvent.VK_PAGE_DOWN
             NativeKeyEvent.VC_NUM_LOCK -> KeyEvent.VK_NUM_LOCK
+            NativeKeyEvent.VC_META -> KeyEvent.VK_WINDOWS
+            NativeKeyEvent.VC_CONTEXT_MENU -> KeyEvent.VK_CONTEXT_MENU
             NativeKeyEvent.VC_F1 -> KeyEvent.VK_F1
             NativeKeyEvent.VC_F2 -> KeyEvent.VK_F2
             NativeKeyEvent.VC_F3 -> KeyEvent.VK_F3
@@ -83,6 +84,13 @@ class KeyboardListenerService : ListenerService(), NativeKeyListener {
             NativeKeyEvent.VC_F10 -> KeyEvent.VK_F10
             NativeKeyEvent.VC_F11 -> KeyEvent.VK_F11
             NativeKeyEvent.VC_F12 -> KeyEvent.VK_F12
+        // Character keys
+            NativeKeyEvent.VC_EQUALS -> KeyEvent.VK_ADD
+            NativeKeyEvent.VC_SLASH -> KeyEvent.VK_QUOTE
+            NativeKeyEvent.VC_BACKQUOTE -> event.rawCode
+            NativeKeyEvent.VC_MINUS -> KeyEvent.VK_MINUS
+            NativeKeyEvent.VC_PERIOD -> KeyEvent.VK_PERIOD
+            NativeKeyEvent.VC_COMMA -> KeyEvent.VK_COMMA
             NativeKeyEvent.VC_OPEN_BRACKET -> KeyEvent.VK_S
             NativeKeyEvent.VC_CLOSE_BRACKET -> KeyEvent.VK_D
             NativeKeyEvent.VC_SEMICOLON -> KeyEvent.VK_C
